fa012ad600aa1151365b3a665acc918f2d41a471
Nikhil Kumar Singh <40004129+rinkuniks@users.noreply.github.com>
2025-02-12 14:55:32 +0530

    changes doen for new home

--- LearnFlutter/lib/screens/MyHomePageNew.dart
+++ LearnFlutter/lib/screens/MyHomePageNew.dart
@@ -1,18 +1,220 @@
 import 'package:flutter/material.dart';
 import 'package:learnflutter/Constants/Dimens.dart';
 
-class MyHomePageNew extends StatelessWidget {
-  const MyHomePageNew({super.key});
+import '../Constants/Constants.dart';
+import 'DashBoard.dart';
+
+class MyHomePageNew extends StatefulWidget {
+
+  MyHomePageNew({super.key});
+
+  @override
+  State<MyHomePageNew> createState() => _MyHomePageNewState();
+}
+
+class _MyHomePageNewState extends State<MyHomePageNew> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    DashBoard(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const Image(
+              image: NetworkImage(Constants.StaticImage),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: Dimens.twelve),
+              child: Text(
+                "Welcome \nNikhil Kumar Singh",
+                style: TextStyle(
+                  fontSize: Dimens.sixteen,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Text(
+              "+91-9999912345",
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "john.mclean@examplepetstore.com",
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                  right: Dimens.twelve, left: Dimens.twelve),
+              child: Divider(
+                thickness: 1,
+                color: Colors.black26,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(Dimens.eight),
+              child: Column(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    CircleAvatar(
+                      radius: Dimens.twenty,
+                      backgroundImage: NetworkImage(Constants.StaticImage),
+                    ),
+                    SizedBox(
+                      width: Dimens.twelve,
+                    ),
+                    Text(
+                      "Post Property",
+                      textAlign: TextAlign.center,
+                    ),
+                  ]),
+                  SizedBox(
+                    height: Dimens.twelve,
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    Image.asset(
+                        height: Dimens.thirtyTwo,
+                        width: Dimens.thirtyTwo,
+                        Constants.WhatsAppImage),
+                    SizedBox(
+                      width: Dimens.twelve,
+                    ),
+                    Text(
+                      "Post Property on whatsApp",
+                      textAlign: TextAlign.center,
+                    ),
+                  ]),
+                  SizedBox(
+                    height: Dimens.twelve,
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    CircleAvatar(
+                      radius: Dimens.twenty,
+                      backgroundImage: NetworkImage(Constants.StaticImage),
+                    ),
+                    SizedBox(
+                      width: Dimens.twelve,
+                    ),
+                    Text(
+                      "Search Properties",
+                      textAlign: TextAlign.center,
+                    ),
+                  ]),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                  right: Dimens.twelve, left: Dimens.twelve),
+              child: Divider(
+                thickness: 1,
+                color: Colors.black26,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(Dimens.twelve),
+              child: Column(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    CircleAvatar(
+                      radius: Dimens.twenty,
+                      backgroundImage: NetworkImage(Constants.StaticImage),
+                    ),
+                    SizedBox(
+                      width: Dimens.twelve,
+                    ),
+                    Text(
+                      "HomePage",
+                      textAlign: TextAlign.center,
+                    ),
+                  ]),
+                  Divider(
+                    thickness: 1,
+                    color: Colors.black26,
+                  ),
+                  SizedBox(
+                    height: Dimens.twelve,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset(
+                            height: Dimens.twentyEight,
+                            width: Dimens.twentyEight,
+                            Constants.WhatsAppImage),
+                        Image.asset(
+                            height: Dimens.twentyEight,
+                            width: Dimens.twentyEight,
+                            Constants.InstaImage),
+                        Image.asset(
+                            height: Dimens.twentyEight,
+                            width: Dimens.twentyEight,
+                            Constants.YoutubeImage),
+                      ]),
+                  SizedBox(height: Dimens.twelve),
+                  Divider(
+                    thickness: 1,
+                    color: Colors.black26,
+                  ),
+                  SizedBox(height: Dimens.twelve),
+                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                    Image.asset(
+                        height: Dimens.twentyEight,
+                        width: Dimens.twentyEight,
+                        Constants.Logout),
+                    SizedBox(
+                      width: Dimens.twelve,
+                    ),
+                    Text(
+                      "Sign In / Sign Out",
+                      textAlign: TextAlign.center,
+                    ),
+                  ]),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
       body: CustomScrollView(
         slivers: [
           //Sliver App Bar
           SliverAppBar(
             backgroundColor: Colors.deepPurple[300],
-            leading: Icon(Icons.menu),
             expandedHeight: Dimens.twoHundred,
             floating: true,
             pinned: true,
@@ -73,5 +275,11 @@ class MyHomePageNew extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _onItemTapped(int value) {
+    setState(() {
+      _selectedIndex = value;
+    });
   }
 }
--- LearnFlutter/lib/screens/PostProperty.dart
+++ LearnFlutter/lib/screens/PostProperty.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:learnflutter/screens/EnterMobileScreen.dart';
 
 class Postproperty extends StatefulWidget {
   const Postproperty({super.key});
