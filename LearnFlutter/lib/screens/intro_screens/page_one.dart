f4b4839e734b9deffc134e4e78efaf26cab87b26
Nikhil Kumar Singh <40004129+rinkuniks@users.noreply.github.com>
2025-02-14 11:30:18 +0530

    Animation on Tap

--- LearnFlutter/lib/screens/intro_screens/page_one.dart
+++ LearnFlutter/lib/screens/intro_screens/page_one.dart
@@ -1,16 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class IntroPageOne extends StatelessWidget {
+/*
+* Animation Controller is used where we want to animate something
+* on Tap it will work
+* rest in Intro page Two and Three Normal animation can be seen
+* */
+
+class IntroPageOne extends StatefulWidget {
   const IntroPageOne({super.key});
+
+  @override
+  State<IntroPageOne> createState() => _IntroPageOneState();
+}
+
+class _IntroPageOneState extends State<IntroPageOne>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  bool bookMarked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[100],
       body: Center(
-        child: Lottie.network(
-            "https://lottie.host/91299220-268e-4263-bb60-c4e96b7b619f/7alAGWIlgj.json"),
+        child: GestureDetector(
+          onTap: () {
+            if (bookMarked == false) {
+              bookMarked = true;
+              _animationController.forward();
+            } else {
+              bookMarked = false;
+              _animationController.reverse();
+            }
+          },
+          child: Lottie.network(
+            "https://lottie.host/91299220-268e-4263-bb60-c4e96b7b619f"
+            "/7alAGWIlgj.json",
+            controller: _animationController,
+          ),
+        ),
       ),
     );
   }
