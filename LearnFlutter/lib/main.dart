0fc08bacda4edc0d2e4d5fd7724e7717ecc0803c
Nikhil Kumar Singh <40004129+rinkuniks@users.noreply.github.com>
2025-02-21 16:47:56 +0530

    Firebase setup

--- LearnFlutter/lib/main.dart
+++ LearnFlutter/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:learnflutter/firebase_options.dart';
 import 'package:learnflutter/screens/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
