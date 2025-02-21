a4018ea363c63ca702aa4e5859457528e8ba45e2
Nikhil Kumar Singh <40004129+rinkuniks@users.noreply.github.com>
2025-02-21 15:44:53 +0530

    Profile page set

--- LearnFlutter/lib/screens/settings/profile_screen.dart
+++ LearnFlutter/lib/screens/settings/profile_screen.dart
@@ -29,139 +29,134 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       body: Container(
         color: Colors.deepPurple[200],
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: (){
-                Navigator.pop(context);
-              },
-              child: Container(
-                padding: const EdgeInsets.only(
-                    left: Dimens.twentyFour, top: Dimens.fifty),
-                child: Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
+        child: SingleChildScrollView(  // Wrap everything inside the scrollable area
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  padding: const EdgeInsets.only(
+                      left: Dimens.twentyFour, top: Dimens.fifty),
+                  child: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            Expanded(
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: Dimens.fifty,
-                    child: Container(
-                      padding: const EdgeInsets.only(
-                        top: Dimens.eighty,
+              // Image at the top of the profile screen
+              Container(
+                margin: EdgeInsets.all(Dimens.forty),
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  "assets/instagram.png",
+                  height: 100,
+                ),
+              ),
+              // Content area for profile details
+              Container(
+                padding: const EdgeInsets.only(
+                  top: Dimens.twentyFour,
+                  left: Dimens.twentyFour,
+                  right: Dimens.twentyFour,
+                ),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Dimens.twentyFour),
+                    color: Colors.white),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    MyTextField(
+                        controller: _firstnameController,
+                        hint: "Enter First Name",
+                        obscureText: false),
+                    const SizedBox(
+                      height: Dimens.eight,
+                    ),
+                    MyTextField(
+                        controller: _lastnameController,
+                        hint: "Enter Last Name",
+                        obscureText: false),
+                    const SizedBox(
+                      height: Dimens.eight,
+                    ),
+                    MyTextField(
+                        controller: _usernameController,
+                        hint: "Enter User Name",
+                        obscureText: false),
+                    const SizedBox(
+                      height: Dimens.eight,
+                    ),
+                    MyTextField(
+                        controller: _emailController,
+                        hint: "Enter Email id",
+                        obscureText: false),
+                    const SizedBox(
+                      height: Dimens.eight,
+                    ),
+                    MyTextField(
+                        controller: _phoneController,
+                        hint: "Enter Phone Number",
+                        obscureText: false),
+                    const SizedBox(
+                      height: Dimens.eight,
+                    ),
+                    Divider(
+                      thickness: 1,
+                    ),
+                    const SizedBox(
+                      height: Dimens.eight,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: Dimens.twentyEight),
+                      child: Text(
+                        "Gender",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Dimens.sixteen),
                       ),
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height,
-                      decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(Dimens.twentyFour),
-                          color: Colors.white),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          MyTextField(
-                              controller: _firstnameController,
-                              hint: "Enter First Name",
-                              obscureText: false),
-                          const SizedBox(
-                            height: Dimens.eight,
+                    ),
+                    const SizedBox(
+                      height: Dimens.eight,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: Dimens.eight),
+                      child: Row(
+                        children: <Widget>[
+                          Radio<String>(
+                            value: 'Male',
+                            groupValue: _selectedGender,
+                            onChanged: _handleGenderChange,
                           ),
-                          MyTextField(
-                              controller: _lastnameController,
-                              hint: "Enter Last Name",
-                              obscureText: false),
-                          const SizedBox(
-                            height: Dimens.eight,
-                          ),
-                          MyTextField(
-                              controller: _usernameController,
-                              hint: "Enter User Name",
-                              obscureText: false),
-                          const SizedBox(
-                            height: Dimens.eight,
-                          ),
-                          MyTextField(
-                              controller: _emailController,
-                              hint: "Enter Email id",
-                              obscureText: false),
-                          const SizedBox(
-                            height: Dimens.eight,
-                          ),
-                          MyTextField(
-                              controller: _phoneController,
-                              hint: "Enter Phone Number",
-                              obscureText: false),
-                          const SizedBox(
-                            height: Dimens.eight,
-                          ),
-                          Divider(
-                            thickness: 1,
-                          ),
-                          const SizedBox(
-                            height: Dimens.eight,
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: Dimens.twentyEight),
-                            child: Text(
-                              "Gender",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Dimens.sixteen),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: Dimens.eight,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: Dimens.eight),
-                            child: Row(
-                              children: <Widget>[
-                                Radio<String>(
-                                  value: 'Male',
-                                  groupValue: _selectedGender,
-                                  onChanged: _handleGenderChange,
-                                ),
-                                Text('Male'),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: Dimens.eight),
-                            child: Row(
-                              children: <Widget>[
-                                Radio<String>(
-                                  value: 'Female',
-                                  groupValue: _selectedGender,
-                                  onChanged: _handleGenderChange,
-                                ),
-                                Text('Female'),
-                              ],
-                            ),
-                          ),
-                          Divider(thickness: 1,),
-                          MyButton(btnText: "Logout", onTap: (){})
+                          Text('Male'),
                         ],
                       ),
                     ),
-                  ),
-                  Container(
-                      alignment: Alignment.topCenter,
-                      child: Image.asset(
-                        "assets/instagram.png",
-                        height: 100,
-                      )),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: Dimens.eight),
+                      child: Row(
+                        children: <Widget>[
+                          Radio<String>(
+                            value: 'Female',
+                            groupValue: _selectedGender,
+                            onChanged: _handleGenderChange,
+                          ),
+                          Text('Female'),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      thickness: 1,
+                    ),
+                    MyButton(btnText: "Logout", onTap: (){})
+                  ],
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
